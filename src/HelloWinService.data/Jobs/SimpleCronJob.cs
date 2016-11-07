8d6d97f79d01eb18c0f8b49632176798abf1afbd
elandcloud <xiaoxm_001@outlook.com>
2016-11-07 14:52:51 +0800

    hello winservice

---
+++ src/HelloWinService.data/Jobs/SimpleCronJob.cs
@@ -0,0 +1,47 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using RestSharp;
+using HelloWinService.data.Service;
+
+namespace HelloWinService.data.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class SimpleCronJob : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            SimpleService.TestCode();
+        }
+
+        //private void SaveOrder()
+        //{
+        //    string baseUrl = "http://10.202.101.43:81";
+        //    var client = new RestClient(baseUrl);
+        //    var request = new RestRequest("api/orders", Method.POST);
+        //    request.RequestFormat = DataFormat.Json;
+        //    OrderDto dto = new OrderDto
+        //    {
+        //        Name = "liu1",
+        //        Id = count++
+        //    };
+        //    List<OrderDto> dtoList = new List<OrderDto> { dto};
+        //    //var jsonDto = JsonConvert.SerializeObject(dto);
+        //    request.AddBody(dtoList);
+
+        //    try
+        //    {
+        //        // execute the request
+        //        IRestResponse response = client.Execute(request);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //    }
+
+        //}
+
+
+    }
+}
---
+++ src/HelloWinService.data/Jobs/SimpleJob.cs
@@ -0,0 +1,23 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using HelloWinService.data.Service;
+
+namespace HelloWinService.data.Jobs
+{
+    [DisallowConcurrentExecution]
+    public class SimpleJob : IJob
+    {
+
+        public void Execute(IJobExecutionContext context)
+        {
+            SimpleService.TestCode();
+        }
+
+    }
+
+
+}
